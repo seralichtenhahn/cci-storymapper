77f1aebf4f935cb8ee1a8f3f151c5e1f2d2580d1
Serafin Lichtenhahn <s.lichtenhahn0420201@arts.ac.uk>
2022-06-16 15:59:45 +0100

    add AboutModal

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import AppContext from "@/components/App/AppContext"
 import AppNavigation from "@/components/App/AppNavigation"
-import Editor from "./components/Editor/Editor"
+import Editor from "@/components/Editor/Editor"
 import Map from "@/components/Map/Map"
+import AboutModal from "@/components/About/AboutModal"
 
 function App() {
   return (
     <AppContext>
       <div className="flex flex-col h-screen">
         <AppNavigation />
-        <div className="flex flex-col-reverse w-full h-full lg:flex-row">
+        <div className="flex flex-none flex-col-reverse w-full h-[calc(100vh-5rem)] lg:flex-row">
           <div className="flex-grow h-full shadow-lg">
             <Editor />
           </div>
@@ -17,6 +18,7 @@ function App() {
           </div>
         </div>
       </div>
+      <AboutModal />
     </AppContext>
   )
 }
