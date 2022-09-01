bf64694fda0f470c6c80a12c83923f021d824996
Serafin Lichtenhahn <s.lichtenhahn0420201@arts.ac.uk>
2022-09-01 20:12:20 +0200

    rename entrypoint

--- apps/api/main.py
+++ apps/api/main.py
@@ -1,4 +1,23 @@
-from src.app import app
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from src._parser import parse_query
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+CORS(app)
+
+app.config.from_pyfile('src/settings.py')
  
-if __name__ == "__main__":
-  app.run()
+@app.route("/parser", methods=["POST"])
+def home_view():
+  if not "query" in request.json:
+    return jsonify({"error": "No query provided"}), 400
+
+  query = request.json["query"]
+  excluded = request.json["excluded"] if "excluded" in request.json else []
+
+  results = parse_query(query, excluded=excluded)
+
+  return jsonify(results)
